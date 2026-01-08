a9590dbd46f2db0c51378bfc571d5cc15b9d9a1d
Himplant <ray@penuma.com>
2026-01-08 07:41:06 -0800

    Add 'https://lovableproject.com' to allowed origins

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const ALLOWED_ORIGINS = [
   "https://eligibility.himplant.com",
   "https://himplant.com",
   "https://www.himplant.com",
+  "https://lovableproject.com",
   "https://lovable.dev",
   "https://himplanteligibility.lovable.app",
 ];
